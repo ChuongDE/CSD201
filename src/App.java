f5615ebbf05995a906b9d2544a0ee992d732c58c
ChuongDe <chuongpntde160221@fpt.edu.vn>
2022-09-29 21:28:51 +0700

    dev commit

--- src/App.java
+++ src/App.java
@@ -1,5 +1,7 @@
 public class App {
     public static void main(String[] args) {
         System.out.println("Chuong Pham");
+        System.out.println("Pham Nguyen Thien Chuong");
+        System.out.println("Le Minh Hoang");
     }
 }
